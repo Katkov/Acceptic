7a1a023fc26be510d7fe0bd624c9295652c4da89
michaelkatkov <eloorus@gmail.com>
2018-11-23 08:00:37 -0800

    Optimized imports

--- app/src/main/java/com/katkov/acceptic/DownloadService.java
+++ app/src/main/java/com/katkov/acceptic/DownloadService.java
@@ -5,7 +5,6 @@ import android.app.IntentService;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Environment;
 import android.os.ResultReceiver;
 
 import java.io.*;
